e3dadec1435d34f2bc9db666b46011cb86637598
vitorsonn <provitor39@gmail.com>
2025-09-04 20:55:02 -0300

    refatoração para mostrar apenas os pratos selecionados na logica main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 let pratosPrincipais = [
     {id: 1, nome: "Bife com batata", price: 30.0},
     {id: 2, nome: "Coxa de frango", price: 25.0},
-    {id: 3, nome: "Carne de panela", price: 22.0}
+    {id: 3, nome: "Carne de panela", price: 22.0},
+    {id: 4, nome: "Farora", price: 10.0},
+    {id: 5, nome: "Salada", price: 8.0},
+    {id: 6, nome: "Torresmo", price: 12.0}
 ]
 
 function calc(){
@@ -9,16 +12,36 @@ function calc(){
     let output = document.getElementById("output")
     let total = 0
 
+    let nomeCliente = document.getElementById("nomeClient").value
+    output.innerHTML = `Caro <strong>${nomeCliente}</strong> </br> Seu Pedido é: </br>`
 
-    output.innerHTML = ""
+    let pratosSelecionados = []
 
     for(let input of quantities){
-        let id = input.id
+        let quantidade = parseFloat(input.value)
+        if (quantidade > 0){
+            let id = input.id
+            let prato = pratosPrincipais[id-1]
+            pratosSelecionados.push({
+                ...prato,
+                quantidade: quantidade
+            })
+        }
 
+    } 
 
-        output.innerHTML += `Prato: ${pratosPrincipais[id-1].nome}  - Preço Unit: ${pratosPrincipais[id-1].price} - Quantidade: ${input.value} </br>`
-        total += pratosPrincipais[id-1].price * parseFloat(input.value)
+    for(let prato of pratosSelecionados){
+        let subtotal = prato.price * prato.quantidade
+        output.innerHTML += `Prato: ${prato.nome}  - Preço Unit: R$${prato.price} - Quantidade: ${prato.quantidade} </br>`
+        total += subtotal
+    }        
+
+    if(pratosSelecionados.length > 0){
+    output.innerHTML += `<h2> Total: R$${total.toFixed(2)} </h2>`
     }
 
-    output.innerHTML += `<h2> Total: ${total} </h2>`
+    else{
+         output.innerHTML += `<p>Nenhum item selecionado.</p>`
+}
+
 }
